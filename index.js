09e8d3cd0f036299f702c0f8e59d5df9c1537ce9
DELHAY <delhayantoine@gmail.com>
2019-01-21 11:20:23 +0100

    Ajout de l'exercice 5

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggle() {
+  var nb1 = document.getElementById("firstNumber").value;
+  var nb2 = document.getElementById("secondNumber").value;
+  alert(Math.floor(nb1)+"\n"+Math.floor(nb2));
+}
